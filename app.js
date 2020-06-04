8f1d449b2af4e27ea9fc34f0648213f75473c980
bolorundurovj <valiantjoshuabolorunduro@gmail.com>
2020-06-04 16:49:50 +0100

    Set Country List

--- app.js
+++ app.js
@@ -8,6 +8,32 @@ app.use(cors());
 
 const port = process.env.PORT || 4910;
 
+let date = new Date();
+let day = date.getUTCDay();
+let year = date.getUTCFullYear();
+let month = date.getUTCMonth();
+let time = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+
+let previousDay = day - 1;
+let correctMonth = month + 1;
+
+console.log("Date: " + date );
+console.log("Day: " + day );
+console.log("Year: " + year );
+console.log("Month: " + month );
+console.log("Time: " + time );
+console.log("Previous Day: " + previousDay );
+
+if(previousDay < 10){
+    previousDay = "0" + previousDay;
+    console.log("Day Checked: " + previousDay );
+}
+if(correctMonth < 10){
+    correctMonth = "0" + correctMonth;
+    console.log("Month: " + correctMonth );
+}
+
+
 app.listen(port, () => {
     console.log(`Live on http://localhost:${port}`);
     
