0bfbc31ea8adafb1faf997d812b4a110eecdbb68
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2021-06-12 09:54:39 +0100

    Add TimeStamps

--- models/Case.js
+++ models/Case.js
@@ -1,46 +1,51 @@
 const mongoose = require('mongoose');
 
-const caseSchema = mongoose.Schema({
-  country: {
-    type: String,
-    required: true,
+const caseSchema = mongoose.Schema(
+  {
+    country: {
+      type: String,
+      required: true,
+    },
+    state: {
+      type: String,
+      default: 'NA',
+    },
+    stateCountry: {
+      type: String,
+      default: 'NA',
+    },
+    lastUpdated: {
+      type: Date,
+      required: true,
+    },
+    activeCases: {
+      type: Number,
+      default: 0,
+    },
+    confirmedCases: {
+      type: Number,
+      default: 0,
+    },
+    fatalCases: {
+      type: Number,
+      default: 0,
+    },
+    recoveredCases: {
+      type: Number,
+      default: 0,
+    },
+    incidentRate: {
+      type: Number,
+      default: 0,
+    },
+    fatalityRatio: {
+      type: Number,
+      default: 0,
+    },
   },
-  state: {
-    type: String,
-    default: 'NA',
-  },
-  stateCountry: {
-    type: String,
-    default: 'NA',
-  },
-  lastUpdated: {
-    type: Date,
-    required: true,
-  },
-  activeCases: {
-    type: Number,
-    default: 0,
-  },
-  confirmedCases: {
-    type: Number,
-    default: 0,
-  },
-  fatalCases: {
-    type: Number,
-    default: 0,
-  },
-  recoveredCases: {
-    type: Number,
-    default: 0,
-  },
-  incidentRate: {
-    type: Number,
-    default: 0,
-  },
-  fatalityRatio: {
-    type: Number,
-    default: 0,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model('Case', caseSchema);
