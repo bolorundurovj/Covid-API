96adb20d0fc3051312c9c00c2f125cff9b6906ae
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2021-06-12 09:53:49 +0100

    Setup Model

---
+++ models/Case.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+const caseSchema = mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    default: 'NA',
+  },
+  stateCountry: {
+    type: String,
+    default: 'NA',
+  },
+  lastUpdated: {
+    type: Date,
+    required: true,
+  },
+  activeCases: {
+    type: Number,
+    default: 0,
+  },
+  confirmedCases: {
+    type: Number,
+    default: 0,
+  },
+  fatalCases: {
+    type: Number,
+    default: 0,
+  },
+  recoveredCases: {
+    type: Number,
+    default: 0,
+  },
+  incidentRate: {
+    type: Number,
+    default: 0,
+  },
+  fatalityRatio: {
+    type: Number,
+    default: 0,
+  },
+});
+
+module.exports = mongoose.model('Case', caseSchema);
