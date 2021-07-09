36fe02699083acebb747bce8b0b9d9bb6230fdf6
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2021-07-09 20:11:32 +0100

    Get Single Country Stats

--- controllers/v2/index.js
+++ controllers/v2/index.js
@@ -1,4 +1,5 @@
-//Get World Stats
+//Get World Stats X
+//Get All Country Stats X
 //Get Country Stats
 //Get Country TimeLine
 //Get Country Geodata
@@ -36,7 +37,7 @@ exports.getWorldStats = async (req, res) => {
   return res.status(200).json(response[0]);
 };
 
-exports.getAllCountries = async (req, res) => {
+exports.getAllCountriesStats = async (req, res) => {
   const response = await Case.aggregate([
     {
       $group: {
@@ -60,6 +61,30 @@ exports.getAllCountries = async (req, res) => {
     {
       $sort: {
         country: 1,
+      },
+    },
+  ]);
+
+  return res.status(200).json(response);
+};
+
+exports.getSingleCountryStats = async (req, res, next) => {
+  let { country } = req.params;
+  country = `^${country}`
+  country = new RegExp(String(country), 'i');
+  const response = await Case.aggregate([
+    {
+      $match: {
+        country: { $regex: country },
+      },
+    },
+    {
+      $unset: ['_id', '__v', 'createdAt', 'updatedAt'],
+    },
+    {
+      $sort: {
+        state: 1,
+        stateCountry: 1,
       },
     },
   ]);
--- routes/v2.js
+++ routes/v2.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getWorldStats, getAllCountries } = require('../controllers/v2');
+const { getWorldStats, getAllCountriesStats, getSingleCountryStats } = require('../controllers/v2');
 
 
 
 router.route('/world-stats').get(getWorldStats);
-router.route('/all-countries').get(getAllCountries);
+router.route('/all-countries-stats').get(getAllCountriesStats);
+router.route('/country-stats/:country').get(getSingleCountryStats);
 
 module.exports = router;
