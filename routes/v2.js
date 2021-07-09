f2515d6ca0e0f2f7faaa39c246d4c7c07851e95f
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2021-07-09 22:29:00 +0100

    Docs: Single Country Stats

--- routes/v2.js
+++ routes/v2.js
@@ -1,11 +1,60 @@
 const express = require('express');
 const router = express.Router();
-const { getWorldStats, getAllCountriesStats, getSingleCountryStats, getAllCountriesTimeline, getSingleCountryTimeline } = require('../controllers/v2');
-
-
+const {
+  getWorldStats,
+  getAllCountriesStats,
+  getSingleCountryStats,
+  getAllCountriesTimeline,
+  getSingleCountryTimeline,
+} = require('../controllers/v2');
 
 router.route('/world-stats').get(getWorldStats);
 router.route('/all-countries-stats').get(getAllCountriesStats);
+
+/**
+ * @route     GET /country-stats/:country
+ * @access    Public
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
+ * For API Documentation
+ * @api {get} /country-stats/:country Get Covid Data By Country
+ * @apiVersion 2.0.0
+ * @apiName Get Covid Data By Country
+ * @apiGroup V2
+ *
+ * @apiDescription Get the latest update on a country
+ *
+ * @apiParam (Url Parameters) {String} country Country Name.
+ *
+ * @apiSuccess (200) {Array} response  Array containing data objects.
+ * @apiSuccessExample {json} Success-Response Example:
+ * HTTP/1.1 200 OK
+ * {
+ *    [
+ *       {
+ *           "location": {
+ *               "type": "Point",
+ *               "coordinates": [
+ *                   8.6753,
+ *                   9.082
+ *               ]
+ *           },
+ *           "state": "",
+ *           "stateCountry": "Nigeria",
+ *           "activeCases": 1580,
+ *           "confirmedCases": 168110,
+ *           "fatalCases": 2122,
+ *           "recoveredCases": 164408,
+ *           "newCases": 0,
+ *           "incidentRate": 81.551536241314,
+ *           "fatalityRatio": 1.2622687526024627,
+ *           "country": "Nigeria",
+ *           "lastUpdated": "2021-07-08T03:21:26.000Z"
+ *       },
+ *   ]
+ *  }
+ */
 router.route('/country-stats/:country').get(getSingleCountryStats);
 
 /**
