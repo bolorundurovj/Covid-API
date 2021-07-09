674db87648c9287d78c09e9057d24f80ccf5c726
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2021-07-09 22:35:05 +0100

    Docs: get All Countries Stats

--- routes/v2.js
+++ routes/v2.js
@@ -9,6 +9,67 @@ const {
 } = require('../controllers/v2');
 
 router.route('/world-stats').get(getWorldStats);
+
+/**
+ * @route     GET /all-countries-stats
+ * @access    Public
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
+ * For API Documentation
+ * @api {get} /all-countries-stats Get All Countries
+ * @apiVersion 2.0.0
+ * @apiName Get All Countries
+ * @apiGroup V2
+ *
+ * @apiDescription Get every country data
+ *
+ * @apiSuccess (200) {Object} response  Response Object containing desired information.
+ * @apiSuccess (200) {Object} response.pagination  Object containning pagination information.
+ * @apiSuccess (200) {Array} response.data  Array containing client objects.
+ * @apiSuccessExample {json} Success-Response Example:
+ * HTTP/1.1 200 OK
+ * [
+ *   {
+ *       "activeCases": 2,
+ *       "confirmedCases": 2530,
+ *       "recoveredCases": 2444,
+ *       "fatalCases": 84,
+ *       "newCases": 0,
+ *       "location": {
+ *           "type": "Point",
+ *           "coordinates": [
+ *               -61.7964,
+ *               17.0608
+ *           ]
+ *       },
+ *       "fatalityRatio": 3.3201581027667983,
+ *       "incidentRate": 1291.7653786455355,
+ *       "country": "Antigua and Barbuda",
+ *       "lastUpdated": "2021-07-08T03:21:26.000Z",
+ *       "subGroups": 2
+ *   },
+ *   {
+ *       "activeCases": 574657,
+ *       "confirmedCases": 9168103,
+ *       "recoveredCases": 8399024,
+ *       "fatalCases": 194422,
+ *       "newCases": 19423,
+ *       "location": {
+ *           "type": "Point",
+ *           "coordinates": [
+ *               -63.6167,
+ *               -38.4161
+ *           ]
+ *       },
+ *       "fatalityRatio": 2.1211150858239747,
+ *       "incidentRate": 10164.142105577697,
+ *       "country": "Argentina",
+ *       "lastUpdated": "2021-07-08T03:21:26.000Z",
+ *       "subGroups": 2
+ *   },
+ *]
+ */
 router.route('/all-countries-stats').get(getAllCountriesStats);
 
 /**
