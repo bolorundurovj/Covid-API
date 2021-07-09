b94ba3df80fe0cbc4f32ba490fa66b039dd30329
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2021-07-09 19:33:10 +0100

    get World Stats

--- controllers/v2/index.js
+++ controllers/v2/index.js
@@ -11,11 +11,41 @@ exports.getWorldStats = async (req, res) => {
   const response = await Case.aggregate([
     {
       $group: {
+        _id: null,
+        activeCases: { $sum: '$activeCases' },
+        confirmedCases: { $sum: '$confirmedCases' },
+        recoveredCases: { $sum: '$recoveredCases' },
+        fatalCases: { $sum: '$fatalCases' },
+        newCases: { $sum: '$newCases' },
+        fatalityRatio: { $last: '$fatalityRatio' },
+        incidentRate: { $last: '$incidentRate' },
+        lastUpdated: { $last: '$lastUpdated' },
+        subGroups: { $sum: 1 },
+      },
+    },
+    {
+      $unset: ['_id'],
+    },
+    {
+      $sort: {
+        country: 1,
+      },
+    },
+  ]);
+
+  return res.status(200).json(response[0]);
+};
+
+exports.getAllCountries = async (req, res) => {
+  const response = await Case.aggregate([
+    {
+      $group: {
         _id: '$country',
         activeCases: { $sum: '$activeCases' },
         confirmedCases: { $sum: '$confirmedCases' },
         recoveredCases: { $sum: '$recoveredCases' },
         fatalCases: { $sum: '$fatalCases' },
+        newCases: { $sum: '$newCases' },
         location: { $last: '$location' },
         fatalityRatio: { $last: '$fatalityRatio' },
         incidentRate: { $last: '$incidentRate' },
@@ -34,5 +64,5 @@ exports.getWorldStats = async (req, res) => {
     },
   ]);
 
-  return res.status(200).json(response)
+  return res.status(200).json(response);
 };
--- routes/v2.js
+++ routes/v2.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getWorldStats } = require('../controllers/v2');
+const { getWorldStats, getAllCountries } = require('../controllers/v2');
 
 
 
 router.route('/world-stats').get(getWorldStats);
-router.route('/all-countries').get(getWorldStats);
+router.route('/all-countries').get(getAllCountries);
 
 module.exports = router;
